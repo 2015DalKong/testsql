dd247b490d7d26e2f5b58b61aa1314b8a2463602
2015DalKong <109940012+2015DalKong@users.noreply.github.com>
2024-09-02 16:14:22 +0900

    test.py

--- test.py
+++ test.py
@@ -1,41 +1,33 @@
 """
 Connects to a SQL database using pymssql
 """
-import pymssql
-import pandas as pd
-import streamlit as st
 
-## MSSQL 접속
-conn = pymssql.connect(
-    server='172.16.0.230',
-    port='49936',
-    user='sa_imn',
-    password='Wdk@0424!@#$',
-    database='IEDATA_IMN',
-    as_dict=True
-)
+import streamlit as st
+import pyodbc
+
+
+@st.cache_resource
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
+    )
+
+conn = init_connection()
+
 cursor = conn.cursor()
 
-## SQL문 실행
-SQL_QUERY = """
-SELECT TOP 10 (ACCNBR) FROM ACCDEF a ;
-"""
-cursor.execute(SQL_QUERY)
+# SQL 쿼리 실행
+cursor.execute("SELECT TOP 10 (ACCNBR) FROM ACCDEF;")
+result = cursor.fetchall()
 
-# records = cursor.fetchall()
-# for r in records:
-#     print(f"{r['ACCNBR']}")
-
-row = cursor.fetchall()
-# while row:
-#     # print(row)
-#     row=cursor.fetchone()
-#     break
+# 결과 표시
+st.write(result)
 
 conn.close()
-
-# print(row)
-df = pd.DataFrame(row)
-print(df)
-
-st.dataframe(df)
